c27607d8f4f3f460f2dd008a6de4ff20fa944db5
ZAPentaleri <zoe@zoetrophy.net>
2025-06-12 01:28:55 -0400

    refactoring, bug fixes, file/folder search improvements

--- src/FileCabinet/SuiteScripts/NextSweep/next-file.js
+++ src/FileCabinet/SuiteScripts/NextSweep/next-file.js
@@ -70,7 +70,7 @@ define(['N/file', 'N/query', 'N/record', 'N/search',], (file, query, record, sea
      * @param {boolean} [options.directChild=true] Search only direct children of base folder
      * @returns {SearchResult[]}
      */
-    function getFolderSearchResults(options) {
+    function querySearchFolder(options) {
         const DEFAULT_FETCH_DEPTH = 24;
         const TAB = '  ';
         const STRING_ESCAPE_SUBSTITUTIONS = [[/</g, '&lt;',], [/>/g, '&gt;',], [/'/g, '\'\'',],];
@@ -161,34 +161,44 @@ define(['N/file', 'N/query', 'N/record', 'N/search',], (file, query, record, sea
     }
 
     /**
-     * Search File Cabinet items by name
+     * Searches the File Cabinet for files or folders (internal interface)
      *
      * @param {object} options
      * @param {string} [options.type=SearchType.FILE]
+     * @param {string|number|string[]|number[]} [options.ids]
      * @param {string} [options.path]
-     * @param {string} [options.name]
      * @param {string|number} [options.baseFolder=0] Base search folder ID (0 = File Cabinet root)
-     * @param {boolean} [options.directChild=false] Search only direct children of base folder
+     * @param {boolean} [options.directChild=true] Search only direct children of base folder
+     * @param {boolean} [options.substring=false]
      * @returns {SearchResult[]}
      */
-    function searchFileCabinet(options) {
+    function searchInternal(options) {
+        //TODO: add support for substring search
         const searchType = options.type ?? SearchType.FILE;
         const searchTypeIsFile = [SearchType.ALL, SearchType.FILE,].includes(searchType);
         const searchTypeIsFolder = [SearchType.ALL, SearchType.FOLDER,].includes(searchType);
-        const pathSegments = options.path ? splitPath(options.path) : [options.name];
+        const itemIds = options.ids ? [].concat(options.ids) : [];
+        const pathSegments = options.path ? splitPath(options.path) : [];
         const baseFolder = options.baseFolder?.toString() ?? '0';
-        const directChild = options.directChild ?? false;
+        const directChild = options.directChild ?? true;
+        const substring = options.substring ?? false;
 
         const baseFolderIsRoot = baseFolder === '0';
         if (directChild && baseFolderIsRoot)
             throw new Error('Files may not be direct children of the File Cabinet root');
+        if (itemIds.length === 0 && pathSegments.length === 0)
+            throw new Error('No valid search query (IDs or path)');
+        if (itemIds.length > 0 && pathSegments.length > 0)
+            throw new Error('IDs and path must not be provided together');
 
         const fileMap = [];
         if (searchTypeIsFile) {
             const fileSearchData = search.create({
                 type: 'file',
-                filters: [...(!baseFolderIsRoot ? [['folder', 'is', baseFolder,], 'AND',] : []),
-                    ['name', 'is', pathSegments.at(-1),],],
+                filters: [
+                    ...(!baseFolderIsRoot ? [['folder', 'is', baseFolder,], 'AND',] : []),
+                    (itemIds.length > 0 ? ['internalid', 'anyof', ...itemIds,] : ['name', 'is', pathSegments.at(-1),]),
+                ],
                 columns: ['name', 'folder', 'filetype',],
             }).runPaged({ pageSize: 1000, });
 
@@ -209,23 +219,24 @@ define(['N/file', 'N/query', 'N/record', 'N/search',], (file, query, record, sea
         const parentResults = [];
         if (searchTypeIsFile) {
             if (pathSegments.length > 1) {
-                parentResults.push(...getFolderSearchResults({
+                parentResults.push(...querySearchFolder({
                     baseFolder: baseFolder,
                     ids: parentFolderIDs,
                     path: pathSegments.slice(0, -1),
                     directChild: directChild,
                 }));
-            } else if (!baseFolderIsRoot) {
-                parentResults.push(...getFolderSearchResults({ ids: parentFolderIDs, }));
+            } else if (parentFolderIDs.length > 0) {
+                parentResults.push(...querySearchFolder({ ids: parentFolderIDs, }));
             }
         }
 
         const parentMap = Object.fromEntries(parentResults.map(result => [result.id, result,]));
 
         return [
-            ...(searchTypeIsFolder ? getFolderSearchResults({
-                path: pathSegments,
+            ...(searchTypeIsFolder ? querySearchFolder({
                 baseFolder: baseFolder,
+                ids: itemIds,
+                path: pathSegments,
                 directChild: directChild,
             }) : []),
             ...fileMap.filter(fileMapping =>
@@ -238,9 +249,10 @@ define(['N/file', 'N/query', 'N/record', 'N/search',], (file, query, record, sea
         ]
     }
 
-    function getFolderId(path) {
-        return getFolderSearchResults({ path: path, baseFolder: 0, })?.[0]?.id ?? null;
-    }
+    function getFolderId(path) { return searchInternal({ path: path, type: SearchType.FOLDER, })?.[0]?.id ?? null; }
+    function getFileId(path) { return searchInternal({ path: path, type: SearchType.FILE, })?.[0]?.id ?? null; }
+    function getFolderPath(id) { return searchInternal({ ids: id, type: SearchType.FOLDER, })?.[0]?.path ?? null; }
+    function getFilePath(id) { return searchInternal({ ids: id, type: SearchType.FILE, })?.[0]?.path ?? null; }
 
     /**
      * Zoe please add details
